f5887e15b063fc8942bf01b946bd47cea767b9aa
Matthew Bunday <zenceph@gmail.com>
2015-02-12 12:02:30 -0500

    Add tests for + operator

--- lisp_test.rb
+++ lisp_test.rb
@@ -3,3 +3,5 @@ require_relative 'lisp'
 program = "(begin (define r 10) (* pi (* r r)))"
 puts tokenize(program) == ['(', 'begin', '(', 'define', 'r', '10', ')', '(', '*', 'pi', '(', '*', 'r', 'r', ')', ')', ')']
 puts parse(program) == [:begin, [:define, :r, 10], [:*, :pi, [:*, :r, :r]]]
+puts eval(parse("(+ 3 5)")) == 8
+puts eval(parse("(+ 3 5 8)")) == 16
