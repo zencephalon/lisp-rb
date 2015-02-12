6ab373832a45e4cba661d45e6e43724b8c9b9f0a
Matthew Bunday <zenceph@gmail.com>
2015-02-12 10:19:29 -0500

    Add read_from_tokens method

--- lisp.rb
+++ lisp.rb
@@ -1,3 +1,19 @@
+# Convert a string into an array of tokens
 def tokenize(str)
   str.gsub('(', ' ( ').gsub(')', ' ) ').split(' ')
+end
+
+# Read an expression from an array of tokens
+def read_from_tokens(tokens)
+  raise SyntaxError, 'unexpected EOF while reading' if tokens.size == 0
+  token = tokens.pop
+  if '(' == token
+    sexp = []
+    sexp.concat(read_from_tokens(tokens)) while tokens.first != ')'
+    tokens.pop # remove the ')'
+  elsif ')' == token
+    raise SyntaxError, 'unexpected )'
+  else
+    return atom(token)
+  end
 end
