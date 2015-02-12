2e917dccdc9ec5a89539ea9843521e5689937011
Matthew Bunday <zenceph@gmail.com>
2015-02-12 12:11:00 -0500

    More arithmetic tests

--- lisp_test.rb
+++ lisp_test.rb
@@ -6,3 +6,5 @@ puts parse(program) == [:begin, [:define, :r, 10], [:*, :pi, [:*, :r, :r]]]
 puts eval(parse("(+ 3 5)")) == 8
 puts eval(parse("(+ 3 5 8)")) == 16
 puts eval(parse("(* 3 5 8)")) == 120
+puts eval(parse("(- 3 5)")) == -2
+puts eval(parse("(/ 6 2)")) == 3
